5dba5c13bf48e095a8127dd09135e62f192d40e3
chayan-DUET <chayan_cse10@yahoo.com>
2015-09-06 00:11:00 +0600

    print fun
    just added a print line

--- Mygitjava/src/mygitjava/Mygitjava.java
+++ Mygitjava/src/mygitjava/Mygitjava.java
@@ -15,5 +15,6 @@ public class Mygitjava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("we are learning github");
     }
 }
