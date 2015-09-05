d2e5f38e102b9f72a9750adeb0bd7a3e6ab457f3
chayan-DUET <chayan_cse10@yahoo.com>
2015-09-06 00:01:31 +0600

    project-initialization
    just i have first project

---
+++ Mygitjava/src/mygitjava/Mygitjava.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygitjava;
+
+/**
+ *
+ * @author Chayan_pc
+ */
+public class Mygitjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
